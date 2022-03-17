5847bac6471dda06e0873ac3f423a674f732e1af
Karen Huang <thinknfix2021@gmail.com>
2022-03-17 23:06:38 +0800

    deploy file修改

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:CodingSnorlax/firm-living.git master:gh-pages
+git push -f https://github.com/CodingSnorlax/firm-living.git master:gh-pages
 
 cd -
